98e9fe8a1b46128ab2adbe1d1c1adcdc3fe740c5
Anto Daniel <anto.daniel@inmobi.com>
2014-05-15 23:19:04 +0530

    modified:   bash/login_host.sh 	modified:   bash/regular_expressions_examples.txt 	bash/tmp 	python/django-ldap-auth/auth_user/

--- bash/login_host.sh
+++ bash/login_host.sh
@@ -1,1 +1,1 @@
-cat /tmp/hosts | while read hostname username passwd;do sshpass -p $passwd ssh $username@$hostname;done
+cat /tmp/hosts | while read hostname username passwd;do sshpass -p $passwd ssh $username@$hostname cat /etc/issue;done
