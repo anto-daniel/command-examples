c63cfc5bc163f887e9a37a791df7b0b9a76b2553
Anto Daniel <anto.daniel@inmobi.com>
2014-06-18 21:08:27 +0530

    perl CBT

--- perl-scripts/fileio1.pl
+++ perl-scripts/fileio1.pl
@@ -1,8 +1,10 @@
 #!/usr/bin/perl -w
 
-$min = 1;
-$max = 20;
-$fullname = "Dean Davis";
+#$min = 1;
+#$max = 20;
+#$fullname = "Anto Daniel";
+@array1 = qw(Anto Rahul Manzoor Ragh Chida Rajini);
+#open (INFILE, "data2") || die "Problems: $!";
+open (OUTFILE, ">>data1") || die "Problems: $!";
 
-open (OUTFILE, ">data1") || die "Problems: $!";
-for ($min..$max) { print OUTFILE "$fullname\n"; }
+foreach (@array1) { print OUTFILE "$_\n"; } 
