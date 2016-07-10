6867ec71d6ea0184e66a4be98061db885904b1c5
Anto Daniel Joseph Paul <APaul@actiance.com>
2016-07-10 23:40:15 +0530

    new file:   fabfile.py

---
+++ python/fabric/fabfile.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from fabric.api import local
+from fabric.api import run
+from fabric.api import env
+
+env.hosts = ['192.168.112.16', '192.168.120.44']
+env.user = "apcuser"
+env.password = "facetime"
+
+def hello(who="world"):
+    print "Hello {who}!".format(who=who)
+
+def prepare_deploy():
+    #local("./manage.py test my_app")
+    local("git add --all && git commit")
+    local("git push")
+
+def test():
+    run('ls')
+
