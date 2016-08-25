8fe336e59b27c9c8fd2639f49a598b719feb4fc7
Anto Daniel <anto.daniel@gmail.com>
2016-08-25 11:44:52 +0530

    fab deployed

--- python/fabric/fabfile.py
+++ python/fabric/fabfile.py
@@ -14,6 +14,7 @@ def hello(who="world"):
 
 def prepare_deploy():
     #local("./manage.py test my_app")
+    local("git pull")
     local("git add --all") 
     local("git config --global user.name 'Anto Daniel'")
     local("git config --global user.email 'anto.daniel@gmail.com'")
