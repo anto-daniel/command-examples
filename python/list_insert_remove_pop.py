4ef20342ace81c668c137e0c0978828cd8ea03d0
Anto Daniel <anto.daniel@gmail.com>
2018-07-13 18:12:26 +0530

    test

--- python/list_insert_remove_pop.py
+++ python/list_insert_remove_pop.py
@@ -8,15 +8,17 @@ for i in range(N):
     a = raw_input()
     if "insert" in a:
         arr.insert(int(a.split()[1]), int(a.split()[2]))
-    if "remove" in a:
+    elif "remove" in a:
         arr.remove(int(a.split()[1]))
-    if "append" in a:
+    elif "append" in a:
         arr.append(int(a.split()[1]))
-    if "sort" in a:
+    elif "sort" in a:
         arr.sort()
-    if "print" in a:
+    elif "print" in a:
         print arr
-    if "pop" in a:
+    elif "pop" in a:
         arr.pop()
-    if "reverse" in a:
+    elif "reverse" in a:
         arr.reverse()
+    else:
+        continue
--- python/percentage.py
+++ python/percentage.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
         name, scores = line[0], line[1:]
         scores = map(float, scores)
         student_marks[name] = scores
+    print("Enter the name of the student to know the score")
     query_name = raw_input()
     a = sum(student_marks[query_name])/3
     print("%.2f" % a)
